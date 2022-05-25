db1478ca1885623a653558febd514a0505a53fc8
Andres <andgonzalezcas@unal.edu.co>
2022-05-25 12:31:56 -0500

    Se realizaron cambios visuales al codigo

--- main.py
+++ main.py
@@ -5,19 +5,23 @@ def input_data():
     filename = input("Ingrese el nombre de su archivo mp3:\n")
     return msg, filename
 
+
 def open_file(filename):
     filepath = "./test/" + filename
     file = open(filepath, "rb")
     return file
+
 
 def menu():
     print("-"*15 + "\n"*2)
     print("Bienvenido" + "\n"*2)
     print("-"*15 + "\n"*2)
 
+
 def hide(msg, file):
     # TODO: steganography
     return True
+
 
 if (__name__=='__main__'):
     menu()
